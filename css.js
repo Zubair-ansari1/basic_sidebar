0e4a7134460ca59a07318cf3b8d1739b31625b9a
Zubair-ansari1 <143747076+Zubair-ansari1@users.noreply.github.com>
2023-09-18 18:04:50 +0530

    added files

---
+++ css.js
@@ -0,0 +1,9 @@
+function openBar() {
+    document.getElementById("side_links").style.left ="0px";
+}
+function closeBar() {
+    document.getElementById("side_links").style.left = "-290px";
+document.getElementById("close").style.transform = "rotate(180deg)";
+}
+
+
